d32723d3d6637f3c4e1dfa7c6dcfd77751afc93a
blacksmithalex <blacksmithalex@mail.ru>
2022-05-07 17:27:40 +0300

    adaptive tasks

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-gsap.fromTo(".intro-text", {opacity:0}, {opacity: 1, duration: 0.5, delay: 0.5});
-gsap.fromTo(".cta1", {opacity:0}, {opacity: 1, duration: 0.5, delay: 1});
-gsap.fromTo(".cta2", {opacity:0}, {opacity: 1, duration: 0.5, delay: 1.25});
-gsap.fromTo(".cta3", {opacity:0}, {opacity: 1, duration: 0.5, delay: 1.5});
-gsap.fromTo(".cta4", {opacity:0}, {opacity: 1, duration: 0.5, delay: 1.75});
-gsap.fromTo(".main-info-wrapper", {opacity:0}, {opacity: 1, duration: 1, delay: 2.5});
-
 const chk = document.getElementById('chk');
 
 chk.addEventListener('change', () => {
@@ -24,6 +17,7 @@ openPopUp.addEventListener('click', function(e){
 closePopUp.addEventListener('click', () => {
     popUp.classList.remove('active');
 })
+
 
 const openProjectsPage = document.getElementById('open_projects_page')
 const projectsPage = document.getElementById('projects_page')
